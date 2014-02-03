d23a2ab13e1eb61baa2abbae370800fd8811d5c5
Miguel Bayon (Toshiba) <miguel.bayon@iessanandres.com>
2014-02-03 16:50:44 +0100

    Primer commit: clase Person creada

---
+++ Person.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Person
+{
+
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person()
+    {
+
+    }
+
+
+}
